b591bb83e32f5675b047fb3e028ecbf2c0cad597
Supunbanupiya <supunbanupiya@gmail.com>
2024-04-04 06:23:49 +0530

    second commit

--- CustomerManagingApp/obj/Debug/net8.0/CustomerManagingApp.AssemblyInfo.cs
+++ CustomerManagingApp/obj/Debug/net8.0/CustomerManagingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerManagingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7cce43aeddd4f9ec1aa7b9267cd811562dfb5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerManagingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerManagingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
